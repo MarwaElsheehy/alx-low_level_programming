d6a72c432cf1d1744e5da09f704727fb4d604bdb
MarwaElsheehy <118805027+MarwaElsheehy@users.noreply.github.com>
2023-03-18 00:18:22 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,22 +9,19 @@
  */
 int main(void)
 {
-	char c;
-
-	char d;
-
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
-		}
+	char c = 'a';
+	char d = 'A';
+	
+	while(c <= 'z') 
+	{
+		putchar(c);
+	        c++;
+	}
+	while(d <= 'Z') 
+	{
+		putchar(d);
+	        d++;
+	}
 	putchar('\n');
 	return (0);
 }

f109cb939d354f5013c8cd270c55ae70bc2ca9ae
MarwaElsheehy <118805027+MarwaElsheehy@users.noreply.github.com>
2023-03-18 00:37:58 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,14 +9,13 @@
  */
 int main(void)
 {
-	int c;
-
-	c = 0;
-	while
-		(c < 10) {
-			putchar(c +'0');
-			c++;
-		}
+	int c = 0;
+	
+	while(c < 10)
+	{
+		putchar(c +'0');
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }

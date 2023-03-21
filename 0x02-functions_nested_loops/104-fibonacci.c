7e3c8cac95ae4a1c74d964c02d0bdb9c4b541c98
MarwaElsheehy <118805027+MarwaElsheehy@users.noreply.github.com>
2023-03-21 23:29:01 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,14 +22,14 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-	for (count = 91; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
 	if (fib_half2 + fib2_half2 > 9999999999)
 	{
-	half += 1;
-	half %= 10000000000;
+	half1 += 1;
+	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)

f01ed62c7e312dd61b535e385db17f37edc7116d
MarwaElsheehy <118805027+MarwaElsheehy@users.noreply.github.com>
2023-04-27 20:01:10 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- * print_list - prints all the elements 
+ *print_list - prints all the elements 
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed

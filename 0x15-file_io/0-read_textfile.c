2caada8bf7f3ad0a5c5950104fec98f62eebba73
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-10 12:31:50 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int i;
+	ssize_t j, k;
+	char *l;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	i = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (i == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	l = malloc(sizeof(char) * (letters));
+	if (!l)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	j = read(i, l, letters);
+	nwr = write(STDOUT_FILENO, l, j);
 
-	close(fd);
+	close(i);
 
-	free(buf);
+	free(l);
 
-	return (nwr);
+	return (k);
 }

a17461161e04ce3a5e4a51f2ede41bdfcbb06df6
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-10 12:40:27 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int i;
+	int j;
+	int k;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
+	if (i == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	for (nletters = 0; text_content[nletters]; nletters++)
+	for (j = 0; text_content[j]; j++)
 		;
 
-	rwr = write(fd, text_content, nletters);
+	k = write(i, text_content, j);
 
-	if (rwr == -1)
+	if (k == -1)
 		return (-1);
 
-	close(fd);
+	close(i);
 
 	return (1);
 }
